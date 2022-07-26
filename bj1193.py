ac572cd2ce57ac3124a7c5c270b658b36bdea960
wldk1217 <wldk1217@naver.com>
2022-07-27 00:27:55 +0900

    20220706

--- bj1193.py
+++ bj1193.py
@@ -1,2 +1,27 @@
-x = int(input())
+from re import I
 
+
+x = int(input())
+i = 1
+while(x >= 0):
+  x -= i
+  if x < 0:
+    x += i
+    i -=1
+    break
+  i += 1
+
+if i % 2 == 1:
+  if x == 0:
+    print('%d/%d' %(1,i))
+  else:
+    m = (i+1) - (x-1)
+    j = x
+    print('%d/%d' %(j,m))
+else:
+  if x == 0:
+    print('%d/%d' %(i,1))
+  else:
+    m = x
+    j = (i+1) - (x-1)
+    print('%d/%d' %(j,m))
